77f18292d569f887e10a0130d5efdb96864e0998
KingCro86 <74135979+KingCro86@users.noreply.github.com>
2021-06-27 11:16:03 +0200

    Završena autorizacija i priprema Nadzorna ploča

--- app/controller/IndexController.php
+++ app/controller/IndexController.php
@@ -1,11 +1,10 @@
 <?php
 
-class IndexController  extends Controller
+class IndexController extends Controller
 {
     public function index()
     {
         $this->view->render('index');
-       
     }
 
     public function era()
@@ -21,6 +20,13 @@ class IndexController  extends Controller
     public function login()
     {
         $this->loginView('','');
+    }
+
+    public function logout()
+    {
+        unset($_SESSION['autoriziran']);
+        session_destroy();
+        $this->index();
     }
 
     public function autorizacija()
@@ -39,6 +45,18 @@ class IndexController  extends Controller
             $this->loginView($_POST['email'],'Obavezno lozinka');
             return;
         }
+
+        //siguran sam da su email i lozinka postavljeni
+        // i sada mogu ići u bazu provjeriti ih - ALI NE DANAS
+        if(!($_POST['email']==='edunova@edunova.hr' && 
+            $_POST['lozinka']==='e') ){
+                $this->loginView($_POST['email'],'Neispravna kombinacija emaila i lozinke');
+                return;
+        }
+
+        $_SESSION['autoriziran']='Edunova Korisnik';
+        $np = new NadzornaplocaController();
+        $np->index();
 
     }
 
