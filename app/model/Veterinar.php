adc373a287dc97f9a6712f3e6df5b805202bc01f
KingCro86 <74135979+KingCro86@users.noreply.github.com>
2021-06-29 11:35:25 +0200

    Završeno čitanje veterinara

--- app/model/Veterinar.php
+++ app/model/Veterinar.php
@@ -10,7 +10,7 @@ class Veterinar
         $izraz=$veza->prepare('
         
         select a.sifra, a.iban,b.ime,b.prezime,
-        b.oib,b.email, count(c.sifra) as ordinacija from veterinar a 
+        b.oib,b.email, count(c.sifra) as ukupnoordinacija from veterinar a 
         inner join osoba b on a.osoba =b.sifra 
         left join ordinacija c on a.sifra =c.veterinar 
         group by a.sifra, a.iban,b.ime,b.prezime,
