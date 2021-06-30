276cbb1f7eb8cdd7301f1d5646a096ba0e131916
KingCro86 <74135979+KingCro86@users.noreply.github.com>
2021-06-30 12:44:16 +0200

    CRUD radnik

---
+++ app/controller/RadnikController.php
@@ -0,0 +1,148 @@
+<?php
+
+class RadnikController extends AutorizacijaController
+{
+    private $viewDir = 'privatno'
+                        . DIRECTORY_SEPARATOR
+                        . 'radnik'
+                        . DIRECTORY_SEPARATOR;
+    
+    private $entitet=null;
+    private $poruka='';
+
+    public function index()
+    {
+        $this->view->render($this->viewDir . 'index',[
+            'entiteti'=>Radnik::ucitajSve()
+        ]);
+    }
+
+    public function novo()
+    {
+        if($_SERVER['REQUEST_METHOD']==='GET'){
+            $this->noviEntitet();
+            return;
+        }
+        $this->entitet = (object) $_POST;
+        try {
+            $this->kontrola();
+            Radnik::dodajNovi($this->entitet);
+            $this->index();
+        } catch (Exception $e) {
+            $this->poruka=$e->getMessage();
+            $this->novoView();
+        }       
+    }
+
+    public function promjena()
+    {
+        if($_SERVER['REQUEST_METHOD']==='GET'){
+            if(!isset($_GET['sifra'])){
+               $ic = new IndexController();
+               $ic->logout();
+               return;
+            }
+            $this->entitet = Radnik::ucitaj($_GET['sifra']);
+            $this->poruka='Promjenite željene podatke';
+            $this->promjenaView();
+            return;
+        }
+        $this->entitet = (object) $_POST;
+        try {
+            $this->kontrolaImePrezime();
+            Radnik::promjeniPostojeci($this->entitet);
+            $this->index();
+        } catch (Exception $e) {
+            $this->poruka=$e->getMessage();
+            $this->promjenaView();
+        }       
+    }
+
+
+    public function brisanje()
+    {
+        if(!isset($_GET['sifra'])){
+            $ic = new IndexController();
+            $ic->logout();
+            return;
+        }
+        Radnik::obrisiPostojeci($_GET['sifra']);
+        header('location: ' . App::config('url') . 'radnik/index');
+       
+    }
+
+
+
+
+
+
+
+    
+
+    private function noviEntitet()
+    {
+        $this->entitet = new stdClass();
+        $this->entitet->ime='';
+        $this->entitet->prezime='';
+        $this->entitet->email='';
+        $this->entitet->oib='';
+        $this->entitet->brojugovora='';
+        $this->poruka='Unesite tražene podatke';
+        $this->novoView();
+    }
+
+    private function promjenaView()
+    {
+        $this->view->render($this->viewDir . 'promjena',[
+            'entitet'=>$this->entitet,
+            'poruka'=>$this->poruka
+        ]);
+    }
+
+
+    private function novoView()
+    {
+        $this->view->render($this->viewDir . 'novo',[
+            'entitet'=>$this->entitet,
+            'poruka'=>$this->poruka
+        ]);
+    }
+
+    private function kontrola()
+    {
+        $this->kontrolaImePrezime();
+        $this->kontrolaOib();
+    }
+
+    private function kontrolaImePrezime()
+    {
+        $this->kontrolaIme();
+        $this->kontrolaPrezime();
+    }
+  
+    private function kontrolaIme()
+    {
+        if(strlen(trim($this->entitet->ime))==0){
+            throw new Exception('Ime obavezno');
+        }
+
+        if(strlen(trim($this->entitet->ime))>50){
+            throw new Exception('Ime predugačko');
+        }
+    }
+
+    private function kontrolaPrezime()
+    {
+        if(strlen(trim($this->entitet->prezime))==0){
+            throw new Exception('Prezime obavezno');
+        }
+    }
+
+    private function kontrolaOib()
+    {
+        if(!Kontrola::CheckOIB($this->entitet->oib)){
+            throw new Exception('OIB nije ispravan');
+        }
+    }
+
+}
